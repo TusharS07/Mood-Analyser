457c22359feda65589ab474e04ffb5041dd6a22e
Salunkhe@07 <salunkhet07@gmail.com>
2022-10-17 17:24:41 +0530

    Message should return HAPPY

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -4,10 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
+    //TC1.1=Given “I am in Sad Mood” message Should Return SAD
     @Test
-    public void checkIfMessageShouldReturnSad() {
+    public void checkIfMessageShouldReturnSAD() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String result = moodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals(result,"SAD");
     }
+
+    //TC1.2 = Given “I am in Any Mood” message Should Return HAPPY
+    @Test
+    public void checkIfMessageShouldReturnHAPPY() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String result = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals(result,"HAPPY");
+    }
+
+
 }
